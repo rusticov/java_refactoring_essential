3db44aa3dc06e0e10bd7b32a8ff306f66380af17
Rusticov <knightdestruction@pm.me>
2026-06-16 19:35:31 +0100

    It works!

--- src/main/java/com/codemanship/refactoring/comments/X1.java
+++ src/main/java/com/codemanship/refactoring/comments/X1.java
@@ -3,12 +3,6 @@ package com.codemanship.refactoring.comments;
 public class X1 {
 
     static int sumSquaresBetweenBounds(int lowerBound, int upperBound) {
-
-        // Return accumulated sum
-        return _sumSquaresBetweenBounds(lowerBound, upperBound);
-    }
-
-    private static int _sumSquaresBetweenBounds(int lowerBound, int upperBound) {
         int result = 0;
         for (int i = lowerBound; i <= upperBound; i++) {
             // Add square of each number in the range
