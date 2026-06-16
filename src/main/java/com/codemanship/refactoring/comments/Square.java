14342bd4ca566ad94ba8d7d679920081b6fe0e10
Rusticov <knightdestruction@pm.me>
2026-06-16 19:41:33 +0100

    It works!

--- src/main/java/com/codemanship/refactoring/comments/Square.java
+++ src/main/java/com/codemanship/refactoring/comments/Square.java
@@ -5,7 +5,6 @@ public class Square {
     static int sumOfSquaresBetweenBounds(int lowerBound, int upperBound) {
         int result = 0;
         for (int i = lowerBound; i <= upperBound; i++) {
-            // Add square of each number in the range
             result += square(i);
         }
         return result;
