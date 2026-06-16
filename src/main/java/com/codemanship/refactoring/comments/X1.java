a557ea16e4102222952d0ceffe25a5e5c288a563
Rusticov <knightdestruction@pm.me>
2026-06-16 19:26:11 +0100

    It works!

--- src/main/java/com/codemanship/refactoring/comments/X1.java
+++ src/main/java/com/codemanship/refactoring/comments/X1.java
@@ -6,12 +6,17 @@ public class X1 {
         int p = 0;
 
         // Iterate from lower bound (q) to upper bound (z)
+        p = sumSquaresBetweenBounds(q, z, p);
+
+        // Return accumulated sum
+        return p;
+    }
+
+    private static int sumSquaresBetweenBounds(int q, int z, int p) {
         for (int i = q; i <= z; i++) {
             // Add square of each number in the range
             p += square(i);
         }
-
-        // Return accumulated sum
         return p;
     }
 
