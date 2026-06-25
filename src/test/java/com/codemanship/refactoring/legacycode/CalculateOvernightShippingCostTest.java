6ff7bbce312c99fb516c312b287f1a4cfbaa40c0
Rusticov <knightdestruction@pm.me>
2026-06-25 23:43:06 +0100

    Add test for overnight shipping of weight 1kg

---
+++ src/test/java/com/codemanship/refactoring/legacycode/CalculateOvernightShippingCostTest.java
@@ -0,0 +1,30 @@
+package com.codemanship.refactoring.legacycode;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CalculateOvernightShippingCostTest {
+
+    @Test
+    @DisplayName("given weight of 1kg when calculating shipping cost then cost is 26.2")
+    void weightOf1kg() throws IOException, InterruptedException {
+        Orders orders = mock(Orders.class);
+        when(orders.fetchOrder(1000)).thenReturn(
+            new Order(
+                1000,
+                "OVERNIGHT",
+                1.0,
+                0.0,
+                false)
+        );
+
+        ShippingCalculator calculator = new ShippingCalculator(orders);
+        assertEquals(26.2, calculator.calculateShipping(1000));
+    }
+}
