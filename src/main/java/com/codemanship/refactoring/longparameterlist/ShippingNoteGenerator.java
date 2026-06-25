ebcf619c0ee3a81e69fed3157a7d017fc8c5216e
Rusticov <knightdestruction@pm.me>
2026-06-25 21:49:29 +0100

    Rename address1 to shippingAddress

--- src/main/java/com/codemanship/refactoring/longparameterlist/ShippingNoteGenerator.java
+++ src/main/java/com/codemanship/refactoring/longparameterlist/ShippingNoteGenerator.java
@@ -4,19 +4,17 @@ public class ShippingNoteGenerator {
 
     public String generateShippingNote(
         Customer customer,
-
-        Address address1, String orderId,
+        Address shippingAddress, String orderId,
         String itemDescription,
         int quantity
     ) {
-
         String fullName = customer.firstName() + " " + customer.lastName();
 
-        String address = address1.addressLine1() + ", "
-                + (address1.addressLine2() != null ? address1.addressLine2() + ", " : "")
-                + address1.city() + ", "
-                + address1.postcode() + ", "
-                + address1.country();
+        String address = shippingAddress.addressLine1() + ", "
+                + (shippingAddress.addressLine2() != null ? shippingAddress.addressLine2() + ", " : "")
+                + shippingAddress.city() + ", "
+                + shippingAddress.postcode() + ", "
+                + shippingAddress.country();
 
         return "SHIPPING NOTE\n"
                 + "Order: " + orderId + "\n"
