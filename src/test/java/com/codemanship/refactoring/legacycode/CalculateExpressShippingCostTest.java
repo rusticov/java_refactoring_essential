4609b29888e69c009848d88018adac6bf5ef9c09
Rusticov <knightdestruction@pm.me>
2026-06-25 23:35:13 +0100

    Add test for express shipping of weight 2kg over 14.0km

--- src/test/java/com/codemanship/refactoring/legacycode/CalculateExpressShippingCostTest.java
+++ src/test/java/com/codemanship/refactoring/legacycode/CalculateExpressShippingCostTest.java
@@ -27,4 +27,21 @@ public class CalculateExpressShippingCostTest {
         ShippingCalculator calculator = new ShippingCalculator(orders);
         assertEquals(1.0, calculator.calculateShipping(1001));
     }
+
+    @Test
+    @DisplayName("given 2kg item over 14km when calculating shipping then cost is 3.0")
+    void weightOf2KgOver14Km() throws IOException, InterruptedException {
+        Orders orders = mock(Orders.class);
+        when(orders.fetchOrder(1001)).thenReturn(
+            new Order(
+                1001,
+                "EXPRESS",
+                2.0,
+                14.0,
+                false)
+        );
+
+        ShippingCalculator calculator = new ShippingCalculator(orders);
+        assertEquals(3.0, calculator.calculateShipping(1001));
+    }
 }
