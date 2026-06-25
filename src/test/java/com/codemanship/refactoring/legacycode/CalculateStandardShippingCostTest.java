18833eb732436ce7300d3227aa9f78a28fec605d
Rusticov <knightdestruction@pm.me>
2026-06-25 23:01:40 +0100

    Replace StubOrders with a Mockito stub

--- src/test/java/com/codemanship/refactoring/legacycode/CalculateStandardShippingCostTest.java
+++ src/test/java/com/codemanship/refactoring/legacycode/CalculateStandardShippingCostTest.java
@@ -2,20 +2,27 @@ package com.codemanship.refactoring.legacycode;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculateStandardShippingCostTest {
 
     @Test
-    void givenStandardShippingAndWeightIs2KgWhenCalculatingCostThenCostIs1() {
-        ShippingCalculator calculator = new ShippingCalculator(new StubOrders(
+    void givenStandardShippingAndWeightIs2KgWhenCalculatingCostThenCostIs1() throws IOException, InterruptedException {
+        Orders orders = mock(Orders.class);
+        when(orders.fetchOrder(1000)).thenReturn(
             new Order(
                 1000,
                 "STANDARD",
                 2.0,
                 0.0,
                 false)
-        ));
+        );
+
+        ShippingCalculator calculator = new ShippingCalculator(orders);
         assertEquals(1.0, calculator.calculateShipping(1000));
     }
 }
