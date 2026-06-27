56b81d6a4dace5bdfe3e04712afed1942769feb8
Rusticov <knightdestruction@pm.me>
2026-06-27 14:11:05 +0100

    ShippingCostStrategy instances are now created one on AvailableShippingCostStrategies and returned by a look-up
    This takes advantage of the fact that the strategies are stateless.

--- src/main/java/com/codemanship/refactoring/legacycode/AvailableShippingCostStrategies.java
+++ src/main/java/com/codemanship/refactoring/legacycode/AvailableShippingCostStrategies.java
@@ -1,13 +1,20 @@
 package com.codemanship.refactoring.legacycode;
 
+import java.util.Map;
+
 public class AvailableShippingCostStrategies {
+
+    private static final Map<String, ShippingCostStrategy> availableStrategies = Map.of(
+            "STANDARD", new StandardShippingCostStrategy(),
+            "EXPRESS", new ExpressShippingCostStrategy(),
+            "OVERNIGHT", new OvernightShippingCostStrategy(),
+            "INTERNATIONAL", new InternationalShippingCostStrategy()
+    );
+
     static ShippingCostStrategy forShippingType(String shippingType) {
-        return switch (shippingType) {
-            case "STANDARD" -> new StandardShippingCostStrategy();
-            case "EXPRESS" -> new ExpressShippingCostStrategy();
-            case "OVERNIGHT" -> new OvernightShippingCostStrategy();
-            case "INTERNATIONAL" -> new InternationalShippingCostStrategy();
-            default -> throw new RuntimeException("Unknown shipping type: " + shippingType);
-        };
+        if (availableStrategies.containsKey(shippingType)) {
+            return availableStrategies.get(shippingType);
+        }
+        throw new RuntimeException("Unknown shipping type: " + shippingType);
     }
 }
