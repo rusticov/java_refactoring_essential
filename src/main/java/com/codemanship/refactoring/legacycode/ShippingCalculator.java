e1f1aca71eafc5d101c0c6d87e6aa4ae5304ff6e
Rusticov <knightdestruction@pm.me>
2026-06-27 12:17:03 +0100

    Extract method calculateOrderCost from `ShippingCalculator.calculateShipping` as a prelude to converting into into a strategy

--- src/main/java/com/codemanship/refactoring/legacycode/ShippingCalculator.java
+++ src/main/java/com/codemanship/refactoring/legacycode/ShippingCalculator.java
@@ -9,25 +9,26 @@ public class ShippingCalculator {
     }
 
     public double calculateShipping(int orderId) {
-
         try {
             Order order = orders.fetchOrder(orderId);
-
-            return switch (order.getShippingType()) {
-                case "STANDARD" -> order.getWeightKg() * 0.5;
-                case "EXPRESS" -> order.getWeightKg() * 0.8
-                    + order.getDistanceKm() * 0.1;
-                case "OVERNIGHT" -> order.getWeightKg() * 1.2 + 25;
-                case "INTERNATIONAL" -> order.getWeightKg() * 1.5;
-                default -> throw new RuntimeException(
-                    "Unknown shipping type: "
-                        + order.getShippingType()
-                );
-            };
-
+            return calculateOrderCost(order);
         } catch (Exception e) {
             System.out.println(e);
             return -1;
         }
+    }
+
+    private static double calculateOrderCost(Order order) {
+        return switch (order.getShippingType()) {
+            case "STANDARD" -> order.getWeightKg() * 0.5;
+            case "EXPRESS" -> order.getWeightKg() * 0.8
+                + order.getDistanceKm() * 0.1;
+            case "OVERNIGHT" -> order.getWeightKg() * 1.2 + 25;
+            case "INTERNATIONAL" -> order.getWeightKg() * 1.5;
+            default -> throw new RuntimeException(
+                "Unknown shipping type: "
+                    + order.getShippingType()
+            );
+        };
     }
 }
